901f970587a0b9f5dc6092c0441a1a8d95694711
haroon47 <haroonfazal@outlook.com>
2018-12-10 20:27:41 +0500

    Initial commit

---
+++ app/src/main/java/com/haroonstudios/internaltutorial2/MainActivity.java
@@ -0,0 +1,121 @@
+package com.haroonstudios.internaltutorial2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonPickImage(View v)
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i,"Select Image"),101);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101 && resultCode == RESULT_OK && data!=null)
+        {
+            Uri uri = data.getData();
+
+            //  NAME of file,,,, PATH of file
+
+            String path = getRealPathFromURI(this,uri);
+            String name = getFileName(uri);
+
+
+            try {
+                insertInPrivateStorage(name,path);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void insertInPrivateStorage(String name, String path) throws IOException {
+        FileOutputStream fos  = openFileOutput(name,MODE_APPEND);
+
+        File file = new File(path);
+
+        byte[] bytes = getBytesFromFile(file);
+
+        fos.write(bytes);
+        fos.close();
+
+        Toast.makeText(getApplicationContext(),"File saved in :"+ getFilesDir() + "/"+name,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    private byte[] getBytesFromFile(File file) throws IOException {
+        byte[] data = FileUtils.readFileToByteArray(file);
+        return data;
+
+    }
+
+    private String getFileName(Uri uri)
+    {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    private String getRealPathFromURI(Context context, Uri uri)
+    {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(uri, proj, null, null,
+                null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return null;
+    }
+
+
+}
